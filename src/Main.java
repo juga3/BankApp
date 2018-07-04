649515b44e14dcf31258ae2201ac623ddf63c033
Stefan Juganaru <32988479+juga3@users.noreply.github.com>
2018-07-04 10:25:28 +0300

    third try

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-import java.io.File;
 import java.io.FileNotFoundException;
 
 class Sortbyinterest implements Comparator<Investor>
@@ -17,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<Investor> investors = new ArrayList<Investor>();
+
 /*        System.out.println("Enter investors number: ");
         int number = input.nextInt();
 
@@ -67,19 +70,100 @@ public class Main {
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        boolean valid;
+        String firstName, secondName, mail, phoneNumber;
+        double sum;
+        int loanTime;
         System.out.println("New client: ");
-        System.out.print("First name: ");
-        String firstName = input.next();
-        System.out.print("Second name: ");
-        String secondName = input.next();
-        System.out.print("Email: ");
-        String mail = input.next();
-        System.out.print("Phone number: ");
-        String phoneNumber = input.next();
-        System.out.print("Wanted sum: ");
-        double sum = input.nextDouble();
-        System.out.print("Loan time in months: ");
-        int loanTime = input.nextInt();
+
+        do {
+            valid = true;
+            System.out.print("Insert first name: ");
+            firstName = input.next();
+            for(int i = 0; i < firstName.length() && valid; i++) {
+                char character = firstName.charAt(i);
+                if (!Character.isLetter(character)) {
+                    valid = false;
+                    System.out.println("Invalid first name!");
+                }
+            }
+        }
+        while(!valid);
+        do {
+            valid = true;
+            System.out.print("Insert second name: ");
+            secondName = input.next();
+            for(int i = 0; i < secondName.length() && valid; i++) {
+                char character = secondName.charAt(i);
+                if (!Character.isLetter(character)) {
+                    valid = false;
+                    System.out.println("Invalid second name!");
+                }
+            }
+        }
+        while(!valid);
+
+        do {
+            valid = true;
+            System.out.print("Insert email: ");
+            mail = input.next();
+            int count = 0;
+            if (mail.charAt(0) == '@')
+                valid = false;
+            for (int i = 0; i < mail.length(); i++)
+                 if (mail.charAt(i) == '@')
+                     count++;
+            if (count != 1)
+                valid = false;
+            if (!(mail.endsWith(".com") || mail.endsWith(".ro") || mail.endsWith(".gov") || mail.endsWith(".net")))
+                valid = false;
+            if(!valid)
+                System.out.println("Invalid email address!");
+        }
+        while(!valid);
+
+        do {
+            valid = true;
+            System.out.print("Insert phone number: ");
+            phoneNumber = input.next();
+            if (!phoneNumber.startsWith("07") || phoneNumber.length() != 10)
+                valid = false;
+            for(int i = 0; i < phoneNumber.length() && valid; i++) {
+                char character = phoneNumber.charAt(i);
+                if (!Character.isDigit(character)) {
+                    valid = false;
+                }
+            }
+            if(!valid)
+                System.out.println("Invalid phone number!");
+        }
+        while(!valid);
+
+        do {
+            valid = true;
+            System.out.print("Insert wanted sum: ");
+            sum = input.nextDouble();
+            double investorFunds = 0;
+            for(int i = 0; i < investors.size(); i++)
+                investorFunds += investors.get(i).getSum();
+            if(sum > investorFunds) {
+                    valid = false;
+                    System.out.println("Insufficient funds!");
+            }
+        }
+        while(!valid);
+
+        do {
+            valid = true;
+            System.out.print("Insert time period for the loan: ");
+            loanTime = input.nextInt();
+            if(loanTime < 12) {
+                valid = false;
+                System.out.println("This period is too short!");
+            }
+        }
+        while(!valid);
 
         Client client = new Client(firstName, secondName, mail, sum, phoneNumber, loanTime);
 
